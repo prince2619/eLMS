d213d66ca3b6c1ad8719068c1c38557bac4e3431
Prince Raj <princeraj7173@gmail.com>
2025-04-28 20:02:24 +0530

    render site added

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-_@876m&g2$*55!90p5cvqfsb)_f07n#33vhp2^3ggabcx#zyjr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'blazeclasses-elms.onrender.com', 
+    'localhost',                  
+    '127.0.0.1'                   
+]
 
 
 # Application definition
